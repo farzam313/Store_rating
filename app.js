df632fffd08c2f8d6367ff2b65178497ab1de72a
farzam313 <107337733+farzam313@users.noreply.github.com>
2025-09-14 16:06:28 +0530

    Store Rating system,|| project configuration

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { connectDB, sequelize } = require("./config/db");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send(" By the way, API is running...");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
